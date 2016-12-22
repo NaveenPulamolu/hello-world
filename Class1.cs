abf16244238775f5b0d988b2d3c442c2b4580e81
NaveenPulamolu <naveenp992@gmail.com>
2016-12-22 00:17:08 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritencsEx
+{
+    class Class1
+    {
+        public void a()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a * b;
+            Console.WriteLine("The result is {0}", c);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritencsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.b();
+            Console.ReadLine();
+        }
+    }
+}
